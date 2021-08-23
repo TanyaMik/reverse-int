6b26ae3e4bce8a3790e17f4c297f5bed53333154
TanyaMik <sssladenkaya@gmail.com>
2021-08-23 20:16:50 +0300

    make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString();
+    let array = str.split('');
+    let revArray = array.reverse();
+    let revStr = revArray.join('');
+    return parseInt(revStr);
 }
